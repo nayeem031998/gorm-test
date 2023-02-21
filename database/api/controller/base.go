af49b99326d38f300eef15bf692f61b68f8ca2a8
nayeem <nayeemakhtar371@gmail.com>
2023-02-21 19:05:17 +0530

    change

--- database/api/controller/base.go
+++ database/api/controller/base.go
@@ -1,34 +1,44 @@
 package controller
 
 import (
+	"fmt"
+
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
-
 	"gorm.io/gorm"
 )
 
 type Server struct {
 	DB     *gorm.DB
 	Router *gin.Engine
+	Value  string
 }
 
-func (server *Server) Initialization() {
-	dsn := "host=localhost user=postgres password=123456 dbname=fitpass port=5432"
-	var err error
-	server.DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	server.Router = gin.New()
-	server.Router.Use(gin.Logger())
-	server.Router.Use(gin.Recovery())
-	server.InitRoutes()
+func (server *Server) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, DbName string) {
 
+	var err error
+	dsn := fmt.Sprintf(`host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Shanghai`, DbHost, DbUser, DbPassword, DbName)
+	server.DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		SkipDefaultTransaction: true,
+		PrepareStmt:            true,
+	})
+	if err != nil {
+		fmt.Printf("Cannot connect to %s database", Dbdriver)
+		log.Fatal("This is the error:", err)
+	} else {
+		fmt.Printf("We are connected to the %s database\n", Dbdriver)
+	}
+	gin.SetMode(gin.ReleaseMode)
+	server.Router = gin.New()
+	server.Router.Use(gin.Recovery(), gin.Logger())
+
+	server.InitRoutes()
 }
 
 func (server *Server) Run(addr string) {
+	fmt.Println("Listening to port 1234")
 	log.Fatal(http.ListenAndServe(addr, server.Router))
 }
--- database/api/server.go
+++ database/api/server.go
@@ -2,13 +2,37 @@ package api
 
 import (
 	"database/api/controller"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 var server = controller.Server{}
 
+func doesFileExist(fileName string) {
+      
+	// check if error is "file not exists"
+	if _, err := os.Stat(fileName); err == nil {
+		if err = godotenv.Load(fileName); err != nil {
+			log.Fatalf("Error getting env, not comming through %v", err)
+		} else {
+			fmt.Printf("We are getting the %s values\n", fileName)
+		}
+	}
+}
 func Run() {
-
-	server.Initialization()
-
+	var err error
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error getting env, not comming through %v", err)
+	} else {
+		fmt.Println("We are getting the env values\n")
+	}
+	doesFileExist(".local")
+	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+	fmt.Println("SCHEMA------", os.Getenv("SCHEMA"))
 	server.Run(":1234")
+
 }
